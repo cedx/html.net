f3b70ace0194f41913a5885fd9e9962966dc484c
Cédric Belin <cedx@outlook.com>
2026-02-28 15:28:23 +0100

    Add the `Directionality` enum

---
+++ src/Directionality.cs
@@ -0,0 +1,22 @@
+namespace Belin.Html.Cmdlets;
+
+/// <summary>
+/// Defines the directionality of an element's text.
+/// </summary>
+public enum Directionality {
+
+	/// <summary>
+	/// The user agent decides.
+	/// </summary>
+	Auto,
+
+	/// <summary>
+	/// From left to right.
+	/// </summary>
+	Ltr,
+
+	/// <summary>
+	/// From right to left.
+	/// </summary>
+	Rtl
+}
